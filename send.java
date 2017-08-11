7d541b848262ee2bc2eca1d23d01684878488fc7
clearu <clearlabutsa>
2017-08-11 11:30:47 -0700

    corrected try-catch

--- send.java
+++ send.java
@@ -6,22 +6,25 @@ public class Send {
   private final static String QUEUE_NAME = "hello";
 
   public static void main(String[] argv){
-    /* Create Connection to Server */
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("localhost"); //indicate IP if on a different machine.
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
+    try{
+      /* Create Connection to Server */
+      ConnectionFactory factory = new ConnectionFactory();
+      factory.setHost("localhost"); //indicatefrances
+      IP if on a different machine.
+      Connection connection = factory.newConnection();
+      Channel channel = connection.createChannel();
 
-    /* Declare Queue to Send to */
-    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    Double message = "3.14";
-    channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-    System.out.println(" [x] Sent '" + message + "'");
+      /* Declare Queue to Send to */
+      channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+      Double message = "3.14";
+      channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+      System.out.println(" [x] Sent '" + message + "'");
 
-    /* Close Channel and Connecition */
-    channel.close();
-    connection.close();
-
+      /* Close Channel and Connecition */
+      channel.close();
+      connection.close();
+    } catch(Exception e){
     throws java.io.IOException {
     }
+  }
 }
